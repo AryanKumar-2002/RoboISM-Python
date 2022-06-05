6e738eb575cde2662b9a9363c57d01aea3fe4101
AryanKumar-2002 <98800679+AryanKumar-2002@users.noreply.github.com>
2022-06-06 02:53:57 +0530

    Delete Question 10.py

--- Question 10.py
+++
@@ -1,8 +0,0 @@
-def swap (x, y):
-    x ^= y
-    y ^= x
-    x ^= y
-    return x, y
-
-#for testing 
-#print ("Swapped vales = " + str (swap (int(input("Enter the first number: ")), int(input("Enter the second number: ")))))
